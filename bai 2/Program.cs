8b9c285f310bc13ded6d03ba8cd84ed7cd5daa3e
Drake <140804892+Drake11088@users.noreply.github.com>
2024-05-19 11:58:02 +0700

    Add files via upload
    lab 3

---
+++ bai 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography;
+
+public class Program
+{
+    public void Main(string[] args)
+    {
+        string username = "drake";
+        string password = "12112005";
+        string time = "22/1/2100";
+
+        var sw = new StringWriter();
+        sw.WriteLine("username: " + username);
+        sw.WriteLine("password: " + password);
+        var content = sw.ToString();
+        Console.WriteLine(content);
+
+        var sr = new StreamReader(content);
+        string Line1 = sr.ReadLine();
+        Console.WriteLine(Line1);
+        string Line2 = sr.ReadLine();
+        Console.WriteLine(Line2);
+
+        string Line = sr.ReadLine();
+        while (Line != null)
+        {
+            Console.WriteLine(Line);
+            Line = sr.ReadLine();
+        }
+
+    }
+}
---
+++ lab3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+
+    public void Main(string[] args)
+    {
+        string path = "ThisPC\\E:\\GAM105\\text.txt";
+        using(FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+        {
+            using (StreamWriter sr = new StreamWriter(fs))
+            {
+                sr.WriteLine("Username:  my Username");
+                sr.WriteLine("Password: myPassword");
+            }
+        }
+        using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+        {
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                Console.WriteLine(sr.ReadToEnd());
+            }
+        }
+    }
+}
